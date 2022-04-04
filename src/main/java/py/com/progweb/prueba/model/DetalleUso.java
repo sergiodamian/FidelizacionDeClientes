0d6a3be3b37f45aa125c0c9ac16c1a56ca91dda1
Nelson Ruiz <nruiz@microtechpy.com>
2022-04-03 20:49:39 -0400

    m

--- src/main/java/py/com/progweb/prueba/model/DetalleUso.java
+++ src/main/java/py/com/progweb/prueba/model/DetalleUso.java
@@ -4,10 +4,54 @@
  */
 package py.com.progweb.prueba.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.Entity;
+import javax.persistence.*;
+import lombok.Data;
+
 /**
  *
  * @author nruiz
  */
+@Entity
+@Data
+@Table(name="detalle_uso_de_puntos")
 public class DetalleUso {
+
+    public DetalleUso() {
+    }
     
+    @Id
+    @GeneratedValue(generator = "delalleSec", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "delalleSec", sequenceName = "detalle_uso_de_puntos", allocationSize = 0)
+    @Basic(optional = false)
+    @Column(name="bolsa_id")
+    private Integer detalleId;
+    @Column(name="puntaje_utilizado")
+    private Integer puntajeUtilizado;
+    @ManyToOne(optional=false)
+    @JoinColumn(name="bolsa_id")
+    @JsonBackReference(value="detalle-bolsa")
+    private BolsaPuntos bolsaPuntos;
+    
+    @ManyToOne(optional=false)
+    @JoinColumn(name="uso_puntos_id")
+    @JsonBackReference(value="detalle-usodepuntos")
+    private  UsoPuntos usoPuntos;
+
+    public Integer getDetalleId() {
+        return detalleId;
+    }
+
+    public void setDetalleId(Integer detalleId) {
+        this.detalleId = detalleId;
+    }
+
+    public Integer getPuntajeUtilizado() {
+        return puntajeUtilizado;
+    }
+
+    public void setPuntajeUtilizado(Integer puntajeUtilizado) {
+        this.puntajeUtilizado = puntajeUtilizado;
+    }
 }
--- src/main/java/py/com/progweb/prueba/model/UsoPuntos.java
+++ src/main/java/py/com/progweb/prueba/model/UsoPuntos.java
@@ -4,10 +4,78 @@
  */
 package py.com.progweb.prueba.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.Data;
+
 /**
  *
  * @author nruiz
  */
+@Entity
+@Data
+@Table(name="uso_de_puntos")
 public class UsoPuntos {
+
+    public UsoPuntos() {
+    }
+    @Id
+    @GeneratedValue(generator = "delalleSec", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "delalleSec", sequenceName = "detalle_uso_de_puntos", allocationSize = 0)
+    @Basic(optional = false)
+    @Column(name="uso_puntos_id")
+    private Integer UsoPuntosId;
+    @Column(name="puntos_usados")
+    private Integer PuntosUsados;
+    @Column(name="fecha_uso")
+    private Date FechaUso;
+    
+    @ManyToOne(optional=false)
+    @JoinColumn(name="concepto_uso_id")
+    @JsonBackReference(value="usodepuntos-conceptodeuso")
+    private ConceptoUso conceptoUso;
+    @OneToMany(mappedBy ="usoPuntos", cascade =  CascadeType.ALL)
+    @JsonBackReference(value="detalle-usodepuntos")
+    private List<DetalleUso> detalleUso=null;
+
+    public Integer getUsoPuntosId() {
+        return UsoPuntosId;
+    }
+
+    public void setUsoPuntosId(Integer UsoPuntosId) {
+        this.UsoPuntosId = UsoPuntosId;
+    }
+
+    public Integer getPuntosUsados() {
+        return PuntosUsados;
+    }
+
+    public void setPuntosUsados(Integer PuntosUsados) {
+        this.PuntosUsados = PuntosUsados;
+    }
+
+    public Date getFechaUso() {
+        return FechaUso;
+    }
+
+    public void setFechaUso(Date FechaUso) {
+        this.FechaUso = FechaUso;
+    }
+    
+   
     
 }
